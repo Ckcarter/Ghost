712be2e72ece22ae6ef389721e1ba3622377bedd
Ché Carter <Ch@Home.com>
2025-09-13 13:40:44 -0500

    fixing the Renderer 09/13/25

--- src/main/java/net/rem/ghost/Ghost.java
+++ src/main/java/net/rem/ghost/Ghost.java
@@ -3,6 +3,8 @@ package net.rem.ghost;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.rem.ghost.entity.ModEntities;
+import net.rem.ghost.item.ModItems;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(Ghost.MOD_ID)
