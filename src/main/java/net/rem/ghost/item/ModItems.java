79063e9228ebc209e5fb19d967a80636bb2fbc2f
Ché Carter <Ch@Home.com>
2025-09-13 14:19:12 -0500

    Fixing the ghost 09/13/25

--- src/main/java/net/rem/ghost/item/ModItems.java
+++ src/main/java/net/rem/ghost/item/ModItems.java
@@ -1,18 +1,34 @@
 package net.rem.ghost.item;
 
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
+import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.rem.ghost.Ghost;
 import net.rem.ghost.entity.ModEntities;
 import net.minecraftforge.common.ForgeSpawnEggItem;
+import net.minecraftforge.common.DeferredSpawnEggItem;
 
+
+@Mod.EventBusSubscriber(modid = Ghost.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, Ghost.MOD_ID);
 
     public static final RegistryObject<Item> GHOST_SPAWN_EGG = ITEMS.register("ghost_spawn_egg",
-            () -> new ForgeSpawnEggItem(ModEntities.GHOST, 0xFFFFFF, 0xAAAAFF,
+            () -> new DeferredSpawnEggItem(ModEntities.GHOST, 0xFFFFFF, 0xAAAAFF,
                     new Item.Properties()));
+
+    @SubscribeEvent
+    public static void addToCreativeTab(CreativeModeTabEvent.BuildContents event) {
+        if (event.getTabKey() == CreativeModeTabs.SPAWN_EGGS) {
+            event.accept(GHOST_SPAWN_EGG);
+        }
+    }
+}
+
 }
