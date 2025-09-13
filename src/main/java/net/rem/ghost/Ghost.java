974bf6da7c0fd16c679e9b89524fe4285798fb02
Ché Carter <Ch@Home.com>
2025-09-13 14:42:48 -0500

    Fixing the ghost 09/13/25

--- src/main/java/net/rem/ghost/Ghost.java
+++ src/main/java/net/rem/ghost/Ghost.java
@@ -2,10 +2,11 @@ package net.rem.ghost;
 
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.rem.ghost.entity.ModEntities;
 import net.rem.ghost.item.ModItems;
-
+import net.minecraftforge.common.DeferredSpawnEggItem;
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(Ghost.MOD_ID)
 public class Ghost {
@@ -17,5 +18,10 @@ public class Ghost {
         ModItems.ITEMS.register(modEventBus);
 
         modEventBus.addListener(ModEntities::registerAttributes);
+        modEventBus.addListener(this::commonSetup);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        DeferredSpawnEggItem.initSpawnEggs();
     }
 }
