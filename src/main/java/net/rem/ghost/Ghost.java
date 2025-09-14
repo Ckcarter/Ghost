ae46ba94aef7c306becb51a237fe4c0f9fd50816
Ché Carter <Ch@Home.com>
2025-09-14 18:29:39 -0500

    Fixing the ghost 09/13/25

--- src/main/java/net/rem/ghost/Ghost.java
+++ src/main/java/net/rem/ghost/Ghost.java
@@ -1,12 +1,13 @@
 package net.rem.ghost;
 
+
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.rem.ghost.entity.ModEntities;
-import net.rem.ghost.item.ModItems;
-import net.minecraftforge.common.DeferredSpawnEggItem;
+
+
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(Ghost.MOD_ID)
 public class Ghost {
@@ -15,13 +16,13 @@ public class Ghost {
     public Ghost() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         ModEntities.ENTITY_TYPES.register(modEventBus);
-        ModItems.ITEMS.register(modEventBus);
+
 
         modEventBus.addListener(ModEntities::registerAttributes);
         modEventBus.addListener(this::commonSetup);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
-        event.enqueueWork(DeferredSpawnEggItem::initSpawnEggs);
+
     }
 }
--- src/main/java/net/rem/ghost/events/ModEvents.java
+++ src/main/java/net/rem/ghost/events/ModEvents.java
@@ -1,43 +1,33 @@
 package net.rem.ghost.events;
 
-import net.minecraft.world.SimpleContainer;
-import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.monster.Zombie;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import net.minecraftforge.event.entity.player.PlayerDropsEvent;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.rem.ghost.Ghost;
-import net.rem.ghost.entity.GhostEntity;
-import net.rem.ghost.entity.ModEntities;
+
 
 @Mod.EventBusSubscriber(modid = Ghost.MOD_ID)
 public class ModEvents {
 
     @SubscribeEvent
-    public static void onPlayerDrops(PlayerDropsEvent event) {
-        Player player = event.getEntity();
+    public static void onPlayerDeath(LivingDeathEvent event) {
+        if (!(event.getEntity() instanceof Player player)) {
+            return;
+        }
+
         Level level = player.level();
         if (level.isClientSide) {
             return;
         }
 
-        GhostEntity ghost = ModEntities.GHOST.get().create(level);
-        if (ghost == null) {
-            return;
+        Zombie zombie = EntityType.ZOMBIE.create(level);
+        if (zombie != null) {
+            zombie.moveTo(player.getX(), player.getY(), player.getZ(), player.getYRot(), player.getXRot());
+            level.addFreshEntity(zombie);
         }
-
-        ghost.moveTo(player.getX(), player.getY(), player.getZ(), player.getYRot(), player.getXRot());
-        SimpleContainer inventory = ghost.getInventory();
-
-        int slot = 0;
-        var iterator = event.getDrops().iterator();
-        while (iterator.hasNext() && slot < inventory.getContainerSize()) {
-            ItemEntity itemEntity = iterator.next();
-            inventory.setItem(slot++, itemEntity.getItem().copy());
-            iterator.remove();
-        }
-
-        level.addFreshEntity(ghost);
     }
 }
