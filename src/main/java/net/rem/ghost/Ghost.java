58dd24def2492f491088ecfd0cb7493031c7f4ff
Ché Carter <Ch@Home.com>
2025-09-14 18:52:06 -0500

    Fixing the ghost 09/13/25

--- src/main/java/net/rem/ghost/Ghost.java
+++ src/main/java/net/rem/ghost/Ghost.java
@@ -1,6 +1,8 @@
 package net.rem.ghost;
 
 
+import net.minecraft.world.entity.monster.Zombie;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -10,10 +12,12 @@ import net.rem.ghost.entity.ModEntities;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(Ghost.MOD_ID)
-public class Ghost {
+public class Ghost extends Zombie {
     public static final String MOD_ID = "ghost";
+    private static final Level Ghost = ;
 
     public Ghost() {
+        super(Ghost);
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         ModEntities.ENTITY_TYPES.register(modEventBus);
 
---
+++ src/main/java/net/rem/ghost/events/ModEventBusEvent.java
@@ -0,0 +1,26 @@
+package net.rem.ghost.events;
+
+import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.rem.ghost.Ghost;
+import net.rem.ghost.entity.GhostEntity;
+import net.rem.ghost.entity.ModEntities;
+
+@Mod.EventBusSubscriber(modid = Ghost.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ModEventBusEvent {
+
+    @SubscribeEvent
+    public static void registerLayer(EntityRenderersEvent.RegisterLayerDefinitions event){
+//        event.registerLayerDefinition(ModModelLayers.TRIBBLE_LAYER, TribbleModel::createBodyLayer);
+    }
+
+    @SubscribeEvent
+    public static void registerAttributes(EntityAttributeCreationEvent event) {
+
+        event.put(ModEntities.GHOST.get(), GhostEntity.createAttributes().build());
+
+    }
+
+}
