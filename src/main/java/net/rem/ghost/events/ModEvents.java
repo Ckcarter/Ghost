824e91f80de2a441bf5216488c368a3d8e3b04cc
Ché Carter <Ch@Home.com>
2025-09-15 11:30:33 -0500

    Fixed the ghost 09/15/25

--- src/main/java/net/rem/ghost/events/ModEvents.java
+++ src/main/java/net/rem/ghost/events/ModEvents.java
@@ -1,5 +1,7 @@
 package net.rem.ghost.events;
 
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.monster.Zombie;
 import net.minecraft.world.entity.player.Player;
@@ -17,19 +19,29 @@ public class ModEvents {
 
     @SubscribeEvent
     public static void onPlayerDeath(LivingDeathEvent event) {
-        if (!(event.getEntity() instanceof Player player)) {
+        if (!(event.getEntity() instanceof ServerPlayer player)) {
             return;
         }
-
-        Level level = player.level();
-        if (level.isClientSide) {
-            return;
-        }
+//
+//        Level level = player.level();
+//        if (level.isClientSide) {
+//            return;
+//        }
+        ServerLevel level = player.serverLevel();
 
         GhostEntity ghost = ModEntities.GHOST.get().create(level);
         if (ghost != null) {
             ghost.setPlayerUUID(player.getUUID());
-            ghost.setPlayerName(player.getGameProfile().getName());
+           // ghost.setPlayerName(player.getGameProfile().getName());
+
+
+            String name = player.getGameProfile().getName();
+            if (name == null || name.isEmpty()) {
+                name = player.getName().getString();
+            }
+
+            ghost.setPlayerName(name);
+
             ghost.moveTo(player.getX(), player.getY(), player.getZ(), player.getYRot(), player.getXRot());
             level.addFreshEntity(ghost);
         }
